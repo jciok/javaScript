898ed2aa8f1fa1222479ca8735ef558c0cafa171
Jakub Ciok <jakub.ciok@enigma.com.pl>
2020-04-21 18:55:31 +0200

    refaktoryzacja plus licznik

--- memory/memory.js
+++ memory/memory.js
@@ -1,47 +1,84 @@
-var cards = ["ciri.png", "geralt.png", "jaskier.png", "jaskier.png", "iorweth.png", "triss.png", "geralt.png", "yen.png","ciri.png", "triss.png", "yen.png", "iorweth.png"];
-//alert(cards[4]);
-//console.log(cards);
+var hour = 0;
 
-var c0 =document.getElementById('c0');
-var c1 =document.getElementById('c1');
-var c2 =document.getElementById('c2');
-var c3 =document.getElementById('c3');
+var minutes = 0;
 
-var c4 =document.getElementById('c4');
-var c5 =document.getElementById('c5');
-var c6 =document.getElementById('c6');
-var c7 =document.getElementById('c7');
+var seconds = 0;
 
-var c8 =document.getElementById('c8');
-var c9 =document.getElementById('c9');
-var c10 =document.getElementById('c10');
-var c11 =document.getElementById('c11');
+function timer()
+	{
+        seconds ++;
+        if(seconds==60){
+            seconds=0;
+            minutes++;
+        }          
 
-c0.addEventListener("click", function(){revalCard(0); })
-c1.addEventListener("click", function(){revalCard(1); })
-c2.addEventListener("click", function(){revalCard(2); })
-c3.addEventListener("click", function(){revalCard(3); })
+        if(minutes==60){
+            minutes=0;
+            hour++;
+        }
+        
+        switch(true){
+            case seconds<10&&minutes<10&&hour<10:
+                document.getElementById("timer").innerHTML = "0"+hour+":"+"0"+minutes+":"+"0"+seconds;
+                break;
+            case seconds<10&&minutes<10&&hour>=10:
+                document.getElementById("timer").innerHTML = hour+":"+"0"+minutes+":"+"0"+seconds;
+                break;
+            case seconds<10&&hour<10&&minutes>=10:
+                document.getElementById("timer").innerHTML = "0"+hour+":"+minutes+":"+"0"+seconds;
+                break;
+            case minutes<10&&hour<10&&seconds>=10:
+                document.getElementById("timer").innerHTML = "0"+hour+":"+"0"+minutes+":"+seconds;
+                break;
+            case hour<10&&seconds>=10&&minutes>=10:
+                document.getElementById("timer").innerHTML = "0"+hour+":"+minutes+":"+seconds;
+                break;
+            case minutes<10&&seconds>=10&&hour>=10:
+                document.getElementById("timer").innerHTML = hour+":"+"0"+minutes+":"+seconds;
+                break;
+            case seconds<10&&minutes>=10&&hour>=10:
+                document.getElementById("timer").innerHTML = hour+":"+minutes+":"+"0"+seconds;
+                break;
+        };
+         
+        if(parsLeft!=0) setTimeout("timer()",1000);
+	}
 
-c4.addEventListener("click", function(){revalCard(4); })
-c5.addEventListener("click", function(){revalCard(5); })
-c6.addEventListener("click", function(){revalCard(6); })
-c7.addEventListener("click", function(){revalCard(7); })
+var cards = ["ciri.png", "ciri.png", "geralt.png", "geralt.png", "jaskier.png", "jaskier.png", "iorweth.png", "iorweth.png", "triss.png", "triss.png", "yen.png", "yen.png"];
 
-c8.addEventListener("click", function(){revalCard(8); })
-c9.addEventListener("click", function(){revalCard(9); })
-c10.addEventListener("click", function(){revalCard(10); })
-c11.addEventListener("click", function(){revalCard(11); })
-
+function shuffle(tmpArray) {
+    var j, x, i;
+    for (i = tmpArray.length - 1; i > 0; i--) {
+        j = Math.floor(Math.random() * (i + 1));
+        x = tmpArray[i];
+        tmpArray[i] = tmpArray[j];
+        tmpArray[j] = x;
+    }
+    return tmpArray;
+}
+shuffle(cards);
+console.log(cards);
+for(var i =0; i< cards.length; i++){
+    $('#target').append('<div id="c'+ i +'" class="card" onclick="revalCard('+i+')"></div>')
+}
+$('#target').append('<div class="score"> Turn counter: 0</div>')
+$('#target').append('<div id="timer" class="timer"> 00:00:00</div>')
 var oneVisible=false;
 var turnCounter=0;
 var isHit = false; 
 var firstCardNumber=0;
-var firstCardNumberTmp=12;
+var firstCardNumberTmp=cards.length+1;
 var lock = false;
-var parsLeft=6;
-
+var parsLeft=(cards.length/2);
+var start=false;
 
 function revalCard(nr){
+
+    if(start==false){
+        timer();
+        start=true;
+    }
+    else{}
 
     var opacityValue=$('#c'+nr).css('opacity');
     if((opacityValue!=0 && lock==false) && firstCardNumberTmp!=nr){
@@ -86,10 +123,9 @@ function hideCards(nr1, nr2){
     $('#c'+ nr2).css('opacity', '0');
 
     parsLeft--;
-
     if(parsLeft==0)
     {
-        $('.board').html('<h1>You Win!<br>Done in '+turnCounter+ ' turns</h1>');
+        $('.board').html('<h1>You Win!<br>Done in '+turnCounter+ ' turns, '+minutes+' minutes and '+ seconds+ ' seconds</h1> <br>  <button type="button" class="btn" onclick="window.location.reload();">Play Again!</button>' );
     }
 
     lock=false;
